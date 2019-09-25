83bfbca885e05406db9a4490234165b77a433af2
Ana Nikolova <ani.nik19@gmail.com>
2019-09-25 16:14:43 +0300

    rendered the list

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,28 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends Component {
-  state = { resources: [] };
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
 
-  async componentDidMount() {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
-    );
-    this.setState({ resources: response.data });
-  }
+  //   const fetchResource = async resource => {
+  //     const response = await axios.get(
+  //       `https://jsonplaceholder.typicode.com/${resource}`
+  //     );
+  //     setResources(response.data);
+  //   };
 
-  async componentDidUpdate(prevProsp) {
-    if (prevProsp.resource !== this.props.resource) {
+  useEffect(() => {
+    (async resource => {
       const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+        `https://jsonplaceholder.typicode.com/${resource}`
       );
-      this.setState({ resources: response.data });
-    }
-  }
+      setResources(response.data);
+    })(resource);
+  }, [resource]);
 
-  render() {
-    return <div>{this.state.resources.length}</div>;
-  }
-}
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+// CLASS BASED COMPONENT
+// class ResourceList extends Component {
+//     state = { resources: [] };
+
+//     async componentDidMount() {
+//         const response = await axios.get(
+//             `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//         );
+//         this.setState({ resources: response.data });
+//     }
+
+//     async componentDidUpdate(prevProsp) {
+//         if (prevProsp.resource !== this.props.resource) {
+//             const response = await axios.get(
+//                 `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//             );
+//             this.setState({ resources: response.data });
+//         }
+//     }
+
+//     render() {
+//         return <div>{this.state.resources.length}</div>;
+//     }
+// }
 
 export default ResourceList;
