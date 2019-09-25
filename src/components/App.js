71d824c4f2ec1a0b5dfdd188f7fedc73905fbac7
Ana Nikolova <ani.nik19@gmail.com>
2019-09-25 11:29:23 +0300

    built Rescource Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
-class App extends Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState("post");
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
