125f715b90e9462eb62f57fa68c76314c6e64a0b
Ana Nikolova <ani.nik19@gmail.com>
2019-09-25 16:24:05 +0300

    code reuse by creatinng useResources function

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ResourceList = ({ resource }) => {
+const useResources = resource => {
   const [resources, setResources] = useState([]);
-
-  //   const fetchResource = async resource => {
-  //     const response = await axios.get(
-  //       `https://jsonplaceholder.typicode.com/${resource}`
-  //     );
-  //     setResources(response.data);
-  //   };
 
   useEffect(() => {
     (async resource => {
@@ -19,7 +12,11 @@ const ResourceList = ({ resource }) => {
       setResources(response.data);
     })(resource);
   }, [resource]);
+  return resources;
+};
 
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
   return (
     <ul>
       {resources.map(record => (
